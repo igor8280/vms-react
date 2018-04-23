44220a403ed9b3acfefadc779de1017bee432a1e
igor <igor8280@gmail.com>
2018-04-23 13:22:31 +0200

    new setup react

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from 'antd';
+import img from '../assets/images/react_logo_512x512.png';
+
+const App = () => {
+  return (
+    <div>
+      <h2 id="heading">Hello ReactJS</h2>
+      <img
+        className="image"
+        style={{ margin: '0.5em' }}
+        height="40"
+        width="40"
+        src={img}
+        alt="React Logo"
+      />
+      <Button type="primary">Primary</Button>
+    </div>
+  );
+};
+
+export default App;
